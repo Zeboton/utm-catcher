ff009978bf92267250c9c5deb59bf5aa8ae2099f
Zeboton <Zeboton@users.noreply.github.com>
2020-08-04 18:28:38 +0300

    Update utm-catcher.js
    Added missed function.

--- utm-catcher.js
+++ utm-catcher.js
@@ -1,15 +1,15 @@
-let lp = {
+let bo = {
     fn: {}
 };
 
-lp.fn.setCookie = function (cname, cvalue, exdays) {
+bo.fn.setCookie = function (cname, cvalue, exdays) {
     let d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/" + ";" + "domain=" + domainSearch(document.location.hostname) + ";";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/" + ";" + "domain=" + bo.fn.domainSearch(document.location.hostname) + ";";
 };
 
-lp.fn.getCookie = function (cname) {
+bo.fn.getCookie = function (cname) {
     let name = cname + "=";
     let ca = document.cookie.split(';');
 
@@ -25,9 +25,9 @@ lp.fn.getCookie = function (cname) {
     return "";
 };
 
-lp.fn.setUTM = function () {
+bo.fn.setUTM = function () {
     const cname = "utm_cookies";
-    let utm = JSON.parse(decodeURIComponent(lp.fn.getCookie(cname) || "%7B%7D"));
+    let utm = JSON.parse(decodeURIComponent(bo.fn.getCookie(cname) || "%7B%7D"));
     let i, u, p, b = /^utm/;
     let us = location.search.replace(/&amp;/g, '&').split(/[?&]/);
     for (i in us) {
@@ -36,5 +36,21 @@ lp.fn.setUTM = function () {
         p = u.split(/=/);
         utm[p[0]] = p[1];
     }
-    lp.fn.setCookie(cname, encodeURIComponent(JSON.stringify(utm)), 31);
+    bo.fn.setCookie(cname, encodeURIComponent(JSON.stringify(utm)), 31);
 };
+
+
+bo.fn.domainSearch = function (domainAsString) {
+    let d = domainAsString.split('.');
+    
+    if (1 == d.length) { // localhost or local
+        return d[0];
+    } else if (2 == d.length) {
+        return d[0] + '.' + d[1];
+    } else if (2 < d.length) {
+        return d[d.length - 2] + '.' + d[d.length - 1];
+    } else {
+        // for unexpectable situation
+        return 'localhost';
+    }
+}
